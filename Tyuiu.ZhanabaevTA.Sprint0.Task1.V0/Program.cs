4b4941d5ca7e0705932fda36a70b8d306971fe4c
Talgat <talgatzhan297@yandex.ru>
2024-09-14 14:58:02 +0500

    Task1

---
+++ Tyuiu.ZhanabaevTA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.ZhanabaevTA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Талгат";
+            int age = 17;
+
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ". Мне " + age + " лет.");
+
+            Console.ReadKey();
+        }
+    }
+}
